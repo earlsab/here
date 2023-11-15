de7421b7b93183589956d59d32164837333e6c10
earlsab <62688314+earlsab@users.noreply.github.com>
2023-11-15 11:05:36 +0800

    Started testing AWS SDK (boto3) and AWS Rekognition
    Rekognition Code from: https://docs.aws.amazon.com/code-library/latest/ug/python_3_rekognition_code_examples.html

--- functions/main.py
+++ functions/main.py
@@ -2,8 +2,12 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
+import logging
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+import boto3
+from botocore.exceptions import ClientError
+logger = logging.getLogger(__name__)
 
 # initialize_app()
 #
@@ -11,3 +15,153 @@ from firebase_admin import initialize_app
 # @https_fn.on_request()
 # def on_request_example(req: https_fn.Request) -> https_fn.Response:
 #     return https_fn.Response("Hello world!")
+
+# TODO: FIND A BETTER WAY TO MAKE THIS SECRET!
+client = boto3.client("rekognition",
+                    aws_access_key_id="AKIA4QSS5UGCCU6UDC5L", 
+                    aws_secret_access_key="TshR7XG4XJbOPaNuyuLm+g35pcbR/eCRgW+eSZZB", 
+                    region_name="ap-southeast-1")
+
+response = client.delete_collection(
+    CollectionId='doc-example-collection-demo'
+)
+print(response)
+
+
+class RekognitionFace:
+    """Encapsulates an Amazon Rekognition face."""
+
+    def __init__(self, face, timestamp=None):
+        """
+        Initializes the face object.
+
+        :param face: Face data, in the format returned by Amazon Rekognition
+                     functions.
+        :param timestamp: The time when the face was detected, if the face was
+                          detected in a video.
+        """
+        self.bounding_box = face.get("BoundingBox")
+        self.confidence = face.get("Confidence")
+        self.landmarks = face.get("Landmarks")
+        self.pose = face.get("Pose")
+        self.quality = face.get("Quality")
+        age_range = face.get("AgeRange")
+        if age_range is not None:
+            self.age_range = (age_range.get("Low"), age_range.get("High"))
+        else:
+            self.age_range = None
+        self.smile = face.get("Smile", {}).get("Value")
+        self.eyeglasses = face.get("Eyeglasses", {}).get("Value")
+        self.sunglasses = face.get("Sunglasses", {}).get("Value")
+        self.gender = face.get("Gender", {}).get("Value", None)
+        self.beard = face.get("Beard", {}).get("Value")
+        self.mustache = face.get("Mustache", {}).get("Value")
+        self.eyes_open = face.get("EyesOpen", {}).get("Value")
+        self.mouth_open = face.get("MouthOpen", {}).get("Value")
+        self.emotions = [
+            emo.get("Type")
+            for emo in face.get("Emotions", [])
+            if emo.get("Confidence", 0) > 50
+        ]
+        self.face_id = face.get("FaceId")
+        self.image_id = face.get("ImageId")
+        self.timestamp = timestamp
+
+    def to_dict(self):
+        """
+        Renders some of the face data to a dict.
+
+        :return: A dict that contains the face data.
+        """
+        rendering = {}
+        if self.bounding_box is not None:
+            rendering["bounding_box"] = self.bounding_box
+        if self.age_range is not None:
+            rendering["age"] = f"{self.age_range[0]} - {self.age_range[1]}"
+        if self.gender is not None:
+            rendering["gender"] = self.gender
+        if self.emotions:
+            rendering["emotions"] = self.emotions
+        if self.face_id is not None:
+            rendering["face_id"] = self.face_id
+        if self.image_id is not None:
+            rendering["image_id"] = self.image_id
+        if self.timestamp is not None:
+            rendering["timestamp"] = self.timestamp
+        has = []
+        if self.smile:
+            has.append("smile")
+        if self.eyeglasses:
+            has.append("eyeglasses")
+        if self.sunglasses:
+            has.append("sunglasses")
+        if self.beard:
+            has.append("beard")
+        if self.mustache:
+            has.append("mustache")
+        if self.eyes_open:
+            has.append("open eyes")
+        if self.mouth_open:
+            has.append("open mouth")
+        if has:
+            rendering["has"] = has
+        return rendering
+    
+
+class RekognitionImage:
+    """
+    Encapsulates an Amazon Rekognition image. This class is a thin wrapper
+    around parts of the Boto3 Amazon Rekognition API.
+    """
+
+    def __init__(self, image, image_name, rekognition_client):
+        """
+        Initializes the image object.
+
+        :param image: Data that defines the image, either the image bytes or
+                      an Amazon S3 bucket and object key.
+        :param image_name: The name of the image.
+        :param rekognition_client: A Boto3 Rekognition client.
+        """
+        self.image = image
+        self.image_name = image_name
+        self.rekognition_client = rekognition_client
+
+
+    def compare_faces(self, target_image, similarity):
+        """
+        Compares faces in the image with the largest face in the target image.
+
+        :param target_image: The target image to compare against.
+        :param similarity: Faces in the image must have a similarity value greater
+                           than this value to be included in the results.
+        :return: A tuple. The first element is the list of faces that match the
+                 reference image. The second element is the list of faces that have
+                 a similarity value below the specified threshold.
+        """
+        try:
+            response = self.rekognition_client.compare_faces(
+                SourceImage=self.image,
+                TargetImage=target_image.image,
+                SimilarityThreshold=similarity,
+            )
+            matches = [
+                RekognitionFace(match["Face"]) for match in response["FaceMatches"]
+            ]
+            unmatches = [RekognitionFace(face) for face in response["UnmatchedFaces"]]
+            logger.info(
+                "Found %s matched faces and %s unmatched faces.",
+                len(matches),
+                len(unmatches),
+            )
+        except ClientError:
+            logger.exception(
+                "Couldn't match faces from %s to %s.",
+                self.image_name,
+                target_image.image_name,
+            )
+            raise
+        else:
+            return matches, unmatches
+
+
