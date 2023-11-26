ce3b8ca65db70c3430a117bcd4b3b23a1de52456
earlsab <62688314+earlsab@users.noreply.github.com>
2023-11-26 18:56:39 +0800

    BROKEN: Faces aren't still detected

---
+++ lib/screens/camera_test.dart
@@ -0,0 +1,230 @@
+// https://docs.flutter.dev/cookbook/plugins/picture-using-camera
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+import 'dart:ui' as ui;
+
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:here/screens/camera/painters/face_detector_painter.dart';
+import 'package:image/image.dart' as img;
+import 'package:logger/logger.dart';
+import 'dart:ui' as ui;
+
+// Future<void> main() async {
+//   // Ensure that plugin services are initialized so that `availableCameras()`
+//   // can be called before `runApp()`
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   // Obtain a list of the available cameras on the device.
+//   final cameras = await availableCameras();
+
+//   // Get a specific camera from the list of available cameras.
+//   final firstCamera = cameras.first;
+
+//   runApp(
+//     MaterialApp(
+//       theme: ThemeData.dark(),
+//       home: TakePictureScreen(
+//         // Pass the appropriate camera to the TakePictureScreen widget.
+//         camera: firstCamera,
+//       ),
+//     ),
+//   );
+// }
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a picture')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until the
+      // controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Attempt to take a picture and get the file `image`
+            // where it was saved.
+            final image = await _controller.takePicture();
+            // print(image.path);
+
+            if (!mounted) return;
+
+            // If the picture was taken, display it on a new screen.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to
+                  // the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  DisplayPictureScreen({super.key, required this.imagePath});
+
+  List<Face>? faces;
+
+  Future<ui.Image?> convertAssetToImage(String assetPath) async {
+    final completer = Completer<ui.Image>();
+    final imageProvider = AssetImage(assetPath);
+    final config = ImageConfiguration();
+
+    imageProvider.resolve(config).addListener(
+      ImageStreamListener((ImageInfo info, bool _) {
+        final image = info.image;
+        completer.complete(image);
+      }),
+    );
+    return completer.future;
+  }
+
+  Future<List<Face>?> processFace() async {
+    final options = FaceDetectorOptions(
+      performanceMode: FaceDetectorMode.accurate,
+      enableContours: true,
+      enableLandmarks: true,
+    );
+    final faceDetector = FaceDetector(options: options);
+    // logger.d(faces);
+    print(imagePath);
+    InputImage image = InputImage.fromFile(File(imagePath));
+    faces = await faceDetector.processImage(image);
+    return faces;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // img.Image? inputImage = img.decodeImage(imageFile.readAsBytesSync());
+    Future<List<Face>?> faces = processFace();
+    Future<ui.Image?> image = convertAssetToImage(imagePath);
+    return Scaffold(
+        appBar: AppBar(title: const Text('Display the Picture')),
+        // The image is stored as a file on the device. Use the `Image.file`
+        // constructor with the given path to display the image.
+        body: Column(
+          children: [
+            FutureBuilder(
+                future: Future.wait([image, faces]),
+                builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return CustomPaint(
+                      painter: FacePainter(
+                        image: snapshot.data?[0],
+                        faces: snapshot.data?[1],
+                      ),
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                })
+          ], // child: Container(Image.file(File(imagePath))),
+        ));
+  }
+}
+
+class FacePainter extends CustomPainter {
+  final ui.Image image;
+  final List<Face> faces;
+  final List<Rect> rects = [];
+
+  FacePainter({required this.image, required this.faces}) {
+    for (var i = 0; i < faces.length; i++) {
+      rects.add(faces[i].boundingBox);
+    }
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15.0
+      ..color = Colors.yellow;
+
+    canvas.drawImage(image, Offset.zero, Paint());
+
+    for (var i = 0; i < faces.length; i++) {
+      canvas.drawRect(rects[i], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FacePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
+  }
+}
