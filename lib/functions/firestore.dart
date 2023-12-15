5b1dbade143f5a5c002d7e985f29d33fa60b4108
Meowtys <40790354+Meowtys@users.noreply.github.com>
2023-12-15 17:55:30 +0800

    Integration starting point

--- lib/functions/firestore.dart
+++ lib/functions/firestore.dart
@@ -21,6 +21,66 @@ class FirestoreService {
     }, SetOptions(merge: true));
   }
 
+  // CREATE & UPDATE: add a new group
+  Future<void> addGroup(String groupName, String groupDescription) {
+    // Get the current user's
+    var userID = _auth.currentUser!.uid;
+
+    // Add the group to the 'groups' collection
+    return FirebaseFirestore.instance
+      .collection('groups')
+      .add({
+        'groupName': groupName,
+        'groupDescription': groupDescription,
+        'groupCreated': Timestamp.now(),
+      })
+      .then((docRef) {
+        // Add the group ID and role to the 'userRoles' collection
+        FirebaseFirestore.instance
+          .collection('users')
+          .doc(userID)
+          .set({
+            'groupRoles': {
+              docRef.id: 'Admin'
+            }
+          }, SetOptions(merge: true));
+      });
+
+  // READ: get groups from Firestore
+  Stream<List<DocumentSnapshot>> getGroupsStream() {
+    // Get the current user's ID
+    var userID = _auth.currentUser!.uid;
+
+    // Listen for real-time updates to the user's groupRoles
+    return FirebaseFirestore.instance
+      .collection('users')
+      .doc(userID)
+      .snapshots()
+      .asyncMap((snapshot) async {
+        // Get the IDs of the groups from groupRoles
+        var groupRoles = snapshot.data()?['groupRoles'] as Map<String, dynamic>?;
+        var groupIDs = groupRoles?.keys.toList();
+
+        // If there are no such groups, return an empty list
+        if (groupIDs == null || groupIDs.isEmpty) {
+          return [];
+        } else {
+          // Query the 'groups' collection for the groups with the obtained IDs
+          var groupsQuery = FirebaseFirestore.instance
+            .collection('groups')
+            .where(FieldPath.documentId, whereIn: groupIDs);
+
+          var groupsSnapshot = await groupsQuery.get();
+
+          // Sort the groups by their 'groupCreated' field
+          var groupsDocs = groupsSnapshot.docs;
+          groupsDocs.sort((a, b) => (b['groupCreated'] as Timestamp).compareTo(a['groupCreated'] as Timestamp));
+
+          return groupsDocs;
+        }
+      });
+  }
   
+
 }
 
